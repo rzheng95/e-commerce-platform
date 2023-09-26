92385a0120a3a27ef3521701b50e946f826730fe
Calen Yang <xiaojiany@yahoo.com>
2023-09-25 22:14:09 -0400

    Add email input bar

--- e-commerce-ui/src/app/app.module.ts
+++ e-commerce-ui/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]

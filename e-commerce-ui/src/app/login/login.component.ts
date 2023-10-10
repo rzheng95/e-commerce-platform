f0d9676b17f02f4b964edb9d60ff73eed0d67474
Henry Zheng <henryzheng92@gmail.com>
2023-10-09 22:06:01 -0500

    connected login form to API endpoint

--- e-commerce-ui/src/app/login/login.component.ts
+++ e-commerce-ui/src/app/login/login.component.ts
@@ -1,20 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
   emailForm: FormGroup;
   emailControl: FormControl;
+  errorMessage: string = '';
 
-  constructor() {
-    this.emailControl = new FormControl('example@example.com', [Validators.required, Validators.email]);
+  constructor(private http: HttpClient) {
+    this.emailControl = new FormControl('example@example.com', [
+      Validators.required,
+      Validators.email,
+    ]);
     this.emailForm = new FormGroup({
-      email: this.emailControl
+      email: this.emailControl,
     });
+  }
+
+  onLogin() {
+    this.http
+      .get(
+        `http://localhost:8080/api/v1/users/check-email?email=${this.emailForm.value.email}`
+      )
+      .subscribe(
+        (data) => {
+          console.log(data, this.emailForm.value);
+          this.errorMessage = '';
+        },
+        (error) => {
+          this.errorMessage = error.message;
+        }
+      );
   }
 }

7e7a161c42fa836651ce55f768b927fc442ab08f
Richard Zheng <ruixiangzheng95@gmail.com>
2023-11-17 17:37:54 -0600

    Refactor code.

--- e-commerce-ui/src/app/app-routing.module.ts
+++ e-commerce-ui/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { LoginComponent } from './auth/login/login.component';
+import { PasswordResetComponent } from './auth/password-reset/password-reset.component';
 import { SignupComponent } from './auth/signup/signup.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'password-reset', component: PasswordResetComponent }
 ];
 
 @NgModule({
